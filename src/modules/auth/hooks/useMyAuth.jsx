1f6dab234fe822cb72e2f75890966294c9eb53b4
Mark <replay.mkp@gmail.com>
2024-03-16 00:16:58 +0200

    added logout env var

--- src/modules/auth/hooks/useMyAuth.jsx
+++ src/modules/auth/hooks/useMyAuth.jsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Pages } from "../../router";
+import { getConfig } from "../";
 
 export const useMyAuth = () => {
   const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();
@@ -12,6 +13,8 @@ export const useMyAuth = () => {
   return { isAuthenticated, isLoading, handleLoginWithAuth, handleLogOut }
 }
 
+const { auth0BaseUri } = getConfig()
+
 const redirectLoginOptions = {
     appState: {
         returnTo: Pages.DASHBOARD.path
@@ -20,6 +23,6 @@ const redirectLoginOptions = {
 
 const logoutOptions = {
   logoutParams: {
-    returnTo: window.location.origin,
+    returnTo: auth0BaseUri,
   }
 }
--- src/modules/auth/utils/config.js
+++ src/modules/auth/utils/config.js
@@ -2,9 +2,11 @@ export const getConfig = () => {
   const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN || ''
   const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENTID || ''
   const auth0RedirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL || ''
+  const auth0BaseUri = import.meta.env.VITE_AUTH0_BASE_URL || ''
   return {
     auth0ClientId,
     auth0Domain,
     auth0RedirectUri,
+    auth0BaseUri,
   }
 }
